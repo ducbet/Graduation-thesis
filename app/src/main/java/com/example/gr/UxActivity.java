525640818d7b3c5790e395ba5aa76bffe5594b0b
ducbet <ducbet1996@gmail.com>
2019-04-15 14:08:20 +0700

    ratation vector sensor

--- app/src/main/java/com/example/gr/UxActivity.java
+++ app/src/main/java/com/example/gr/UxActivity.java
@@ -1,13 +1,30 @@
 package com.example.gr;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class UxActivity extends AppCompatActivity {
+    private RotationVectorSensor mRotationVectorSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ux);
+
+        mRotationVectorSensor = new RotationVectorSensor(this);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mRotationVectorSensor.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mRotationVectorSensor.stop();
     }
 }
